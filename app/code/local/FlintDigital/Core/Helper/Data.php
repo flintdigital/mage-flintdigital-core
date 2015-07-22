4ee79f66561170eefcbcf18e63d696e438b6c9a6
Adrian Esquivel <adrian@flintdigital.com>
2015-07-22 08:46:24 -0600

    Adding code for main Flint Digital tab at admin panel

---
+++ app/code/local/FlintDigital/Core/Helper/Data.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * @category   FlintDigital
+ * @package    FlintDigital Core
+ * @author     Adrian Esquivel <adrian@flintdigital.com>
+ */
+class FlintDigital_Core_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
